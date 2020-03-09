ef5d15a1bacd33e820d099add5c39d3a1480ab75
jpbp <jpbp@estudante.ufla.br>
2020-03-09 09:42:30 -0300

    importando o consign

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const consign= require('consign')
 
-
+consign().then('./config/middlewares.js').into(app)
 
 app.listen(5000,()=>{
     console.log('Backend executando...')
