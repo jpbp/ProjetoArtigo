5dc53ac3928977028b0e54d44c69fc64becff103
jpbp <jpbp@estudante.ufla.br>
2020-03-09 09:55:35 -0300

    rotas configuradas

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,3 +1,3 @@
 module.exports= app=>{
-    app.router('/users').post(app.api.user.save)
+    app.route('/users').post(app.api.user.save)
 }
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const consign= require('consign')
 
-consign().then('./config/middlewares.js').into(app)
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/router.js')
+    .into(app)
 
 app.listen(5000,()=>{
     console.log('Backend executando...')
